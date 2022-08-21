5d9d24b589c628fb74694ac9bfe0b6f6d37812f8
Mr.Pratchayanan <Yanperm@users.noreply.github.com>
2022-08-21 21:55:42 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="http://softubon.co.th/assets/coming-soon.jpeg" alt="logo" width="450px" />
+        <h1 class="page-title">หจก.ซอฟต์อุบล</h1>
+        <p class="page-description">สินค้าไอที,พัฒนาซอฟต์แวร์,ประกันภัยทุกประเภท</p>
+        <img src="http://softubon.co.th/assets/logo-amazon-web-services-partner-network.png" alt="Powered by AWS Cloud Computing" width="120px;"/>
+            <a href="https://lin.ee/lW4BTft"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/th.png" alt="เพิ่มเพื่อน" height="36" border="0"/></a>
+           <br/>
+            <nav class="footer-social-links">
+            <a href="#!" class="social-link"><i class="mdi mdi-facebook-box"></i></a>
+            <a href="#!" class="social-link"><i class="mdi mdi-twitter"></i></a>
+            <a href="#!" class="social-link"><i class="mdi mdi-google"></i></a>
+        </nav>
+        <footer>
+        Copyright 2022 นิติบุคคลผู้เสียภาษี 0343564005061
+
+    </footer>
       </header>
     </div>
   );
